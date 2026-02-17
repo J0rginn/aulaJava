65e050aaa85cce7d595f960d74874114c4328cc5
J0rginn <jorge.hssc@gmail.com>
2026-02-17 18:13:16 -0300

    Subindo arquivos para backup

---
+++ BANCO_RASMOO/src/TESTE_METODO.java
@@ -0,0 +1,11 @@
+public class TESTE_METODO {
+    static void main(String[] args) {
+
+        Conta primeriaConta =new Conta();
+        primeriaConta.depositar(1000);
+
+        primeriaConta.sacar(1200);
+
+
+    }
+}
---
+++ conhecendo_a_linguagem/src/Variaveis_NUM_INT.java
@@ -0,0 +1,16 @@
+public class Variaveis_NUM_INT {
+    public static void main(String[] args){
+        byte idadej= 20;
+
+        System.out.println("Esta é a idade de jorge: " + idadej); //Aqui concatenamos o texto com a variavel.
+
+        //byte -8bits
+        //short - 16bits
+        //int -32bits
+        //long-64bits
+
+        int testaExp = 10*5+50+54+88/2;
+
+        System.out.println("O resultado da expressão e :" + testaExp);
+    }
+}
